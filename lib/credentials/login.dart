8d0c2db09f26d46e0631128ccac127aff509bc23
Jahanzaib <m.jahanzaib29@gmail.com>
2021-05-26 18:47:32 +0500

    Initial commit

--- lib/credentials/login.dart
+++ lib/credentials/login.dart
@@ -12,42 +12,47 @@ class _LoginPageState extends State<LoginPage> {
     Size size = MediaQuery.of(context).size;
     return Scaffold(
       // backgroundColor: Color(0xFF9ecff6),
-      extendBodyBehindAppBar: true,
-      body: SafeArea(
-        child: SingleChildScrollView(
-          child: Expanded(
-              child: Container(
-            child: Stack(
-              children: [
-                Positioned(
-                    child: Container(
-                        width: size.width,
-                        height: size.height ,
-                        decoration: BoxDecoration(
-                            borderRadius: BorderRadius.circular(15.0)),
-                        child: Image.asset(
-                          "asset/images/f.gif",
-                          fit: BoxFit.cover,
-                        ))),
-                Positioned(
-                  top: size.height * .4 - 20,
-                    width: size.width * .8,
-                    left: size.width *.1,
-                    child: Container(
+      // extendBodyBehindAppBar: true,
+      body: Column(
+        children: [
+          SingleChildScrollView(
+            child: Expanded(
+                child: Container(
+              child: Stack(
+                children: [
+                  Positioned(
+                      child: Container(
+                          width: size.width,
+                          height: size.height ,
+                          decoration: BoxDecoration(
+                              borderRadius: BorderRadius.circular(15.0)),
+                          child: Image.asset(
+                            "asset/images/f.gif",
+                            fit: BoxFit.cover,
+                          ))),
+                  Positioned(
+                    top: size.height * .4,
+                      width: size.width * .8,
+                      left: size.width *.1,
                       child: Card(
+                        color: Colors.transparent,
+                        shape: BeveledRectangleBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
                         child: Column(
                           children: [
                             TextField(
                               // controller: _snameTextEditingController,
                               decoration: InputDecoration(
-                                filled: true,
-                                focusColor: Color(0xFF9ecff6),
-                                fillColor:  Colors.transparent,
+                                // filled: true,
+                                // focusColor: Color(0xFF9ecff6),
+                                // fillColor:  Colors.transparent,
                                 prefixIcon: Icon(
                                   Icons.person,
+                                  color: Colors.white,
                                 ),
 
-                                // labelStyle: Pallete.khint,
+                                labelStyle:TextStyle(color: Colors.white),
                                 labelText: "User Name",
                                 hintStyle: TextStyle(color: Colors.white),
                                 border: InputBorder.none,
@@ -56,13 +61,15 @@ class _LoginPageState extends State<LoginPage> {
                             TextField(
                               // controller: _snameTextEditingController,
                               decoration: InputDecoration(
-                                filled: true,
-                                focusColor: Color(0xFF9ecff6),
-                                fillColor:  Colors.transparent,
+                                // filled: true,
+                                // focusColor: Color(0xFF9ecff6),
+                                // fillColor:  Colors.transparent,
                                 prefixIcon: Icon(
                                   Icons.person,
+                                  color: Colors.white,
+
                                 ),
-                                // labelStyle: Pallete.khint,
+                                labelStyle:TextStyle(color: Colors.white),
                                 labelText: "User Name",
                                 hintStyle: TextStyle(color: Colors.white),
                                 border: InputBorder.none,
@@ -70,12 +77,12 @@ class _LoginPageState extends State<LoginPage> {
                             ),
                           ],
                         ),
-                      ),
-                    ))
-              ],
-            ),
-          )),
-        ),
+                      ))
+                ],
+              ),
+            )),
+          ),
+        ],
       ),
     );
   }
