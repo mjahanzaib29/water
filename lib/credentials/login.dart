6f8c2df4f627fec30cc61bfe275317d8eb77f79d
Jahanzaib <m.jahanzaib29@gmail.com>
2021-05-26 19:01:51 +0500

    Initial commit

--- lib/credentials/login.dart
+++ lib/credentials/login.dart
@@ -13,76 +13,92 @@ class _LoginPageState extends State<LoginPage> {
     return Scaffold(
       // backgroundColor: Color(0xFF9ecff6),
       // extendBodyBehindAppBar: true,
-      body: Column(
-        children: [
-          SingleChildScrollView(
-            child: Expanded(
-                child: Container(
-              child: Stack(
-                children: [
-                  Positioned(
-                      child: Container(
-                          width: size.width,
-                          height: size.height ,
-                          decoration: BoxDecoration(
-                              borderRadius: BorderRadius.circular(15.0)),
-                          child: Image.asset(
-                            "asset/images/f.gif",
-                            fit: BoxFit.cover,
-                          ))),
-                  Positioned(
-                    top: size.height * .4,
-                      width: size.width * .8,
-                      left: size.width *.1,
-                      child: Card(
-                        color: Colors.transparent,
-                        shape: BeveledRectangleBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        child: Column(
-                          children: [
-                            TextField(
-                              // controller: _snameTextEditingController,
-                              decoration: InputDecoration(
-                                // filled: true,
-                                // focusColor: Color(0xFF9ecff6),
-                                // fillColor:  Colors.transparent,
-                                prefixIcon: Icon(
-                                  Icons.person,
-                                  color: Colors.white,
-                                ),
-
-                                labelStyle:TextStyle(color: Colors.white),
-                                labelText: "User Name",
-                                hintStyle: TextStyle(color: Colors.white),
-                                border: InputBorder.none,
-                              ),
+      body: SingleChildScrollView(
+        child: Expanded(
+            child: Container(
+          child: Stack(
+            children: [
+              Positioned(
+                  child: Container(
+                      width: size.width,
+                      height: size.height,
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(15.0)),
+                      child: Image.asset(
+                        "asset/images/f.gif",
+                        fit: BoxFit.cover,
+                      ))),
+              Positioned(
+                  top: size.height * .4,
+                  width: size.width * .8,
+                  left: size.width * .1,
+                  child: Card(
+                    color: Colors.transparent,
+                    shape: BeveledRectangleBorder(
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Column(
+                      children: [
+                        TextField(
+                          // controller: _snameTextEditingController,
+                          decoration: InputDecoration(
+                            // filled: true,
+                            // focusColor: Color(0xFF9ecff6),
+                            // fillColor:  Colors.transparent,
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Colors.white,
                             ),
-                            TextField(
-                              // controller: _snameTextEditingController,
-                              decoration: InputDecoration(
-                                // filled: true,
-                                // focusColor: Color(0xFF9ecff6),
-                                // fillColor:  Colors.transparent,
-                                prefixIcon: Icon(
-                                  Icons.person,
-                                  color: Colors.white,
 
-                                ),
-                                labelStyle:TextStyle(color: Colors.white),
-                                labelText: "User Name",
-                                hintStyle: TextStyle(color: Colors.white),
-                                border: InputBorder.none,
-                              ),
-                            ),
-                          ],
+                            labelStyle: TextStyle(color: Colors.white),
+                            labelText: "User Name",
+                            hintStyle: TextStyle(color: Colors.white),
+                            border: InputBorder.none,
+                          ),
                         ),
-                      ))
-                ],
-              ),
-            )),
+                        TextField(
+                          // controller: _snameTextEditingController,
+                          decoration: InputDecoration(
+                            // filled: true,
+                            // focusColor: Color(0xFF9ecff6),
+                            // fillColor:  Colors.transparent,
+                            prefixIcon: Icon(
+                              Icons.person,
+                              color: Colors.white,
+                            ),
+                            labelStyle: TextStyle(color: Colors.white),
+                            labelText: "Password",
+                            hintStyle: TextStyle(color: Colors.white),
+                            border: InputBorder.none,
+                          ),
+                        ),
+                        TextButton(
+                          child: new Text(
+                            'SIGN IN',
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          onPressed: () {},
+                        ),
+
+                        InkWell(
+                          child: RichText(
+                            text: TextSpan(children: [
+                              TextSpan(
+                                  text: "Dont't have an account ?",),
+                              TextSpan(
+                                  text: "Sign Up ", )
+                            ]),
+                          ),
+                          onTap: () {
+                            Navigator.pop(context);
+                          },
+                        )
+                      ],
+                    ),
+                  ))
+            ],
           ),
-        ],
+        )),
       ),
     );
   }
